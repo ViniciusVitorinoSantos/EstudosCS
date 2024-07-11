5bc7cac7798c456cffb5762a3e4581703a1ed9b8
Vitorino.Vinicius <viniciusvitorinodossantos@gmail.com>
2024-07-11 18:59:55 -0300

    Source code
    Publicação do código fonte.

---
+++ pooPokemon/Pokemon.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pooPokemonApp
+{
+
+    public class Pokemon
+    {
+        public Pokemon()
+        {
+
+        }
+
+        private string nome;
+
+        public string Nome
+        {
+            get { return this.nome; }
+            set
+            {
+                string texto = value.ToUpper();
+                this.nome = texto;
+            }
+        }
+
+        private string descricao;//Armazena o valor da propriedade Descricao;
+
+        public string Descricao//Representa a caracteristica Descricao do meu pokemon;
+        {
+            get { return descricao; }
+            set { this.descricao = value.ToUpper(); }
+        }
+
+        public void ExibirDadosPokemon()
+        {
+            Console.WriteLine("Nome do Pokemon: " + this.Nome);
+            Console.WriteLine($"Descrição do Pokemon: {this.Descricao} ");
+        }
+
+    }
+}
---
+++ pooPokemon/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace pooPokemonApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Pokemon p = new Pokemon();
+            p.Nome = ("bulbasaulro");
+            p.Descricao = ("O bulbasauro é um pokemon do tipo planta");
+            p.ExibirDadosPokemon();
+            Console.ReadKey();
+        }
+    }
+}
